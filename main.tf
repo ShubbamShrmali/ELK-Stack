d175bcc02b8c62660a8980545c0ba1bb60491330
ShubbamShrmali <111654213+ShubbamShrmali@users.noreply.github.com>
2022-11-03 18:41:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,35 @@ provider "aws" {
 
 
 
-resource "aws_instance" "JenkinDeploy" {
+resource "aws_instance" "ELK" {
     ami = "ami-026b57f3c383c2eec"
     count = "1"
     key_name   = "Jenkins"
     instance_type = "t2.micro"
     security_groups = ["launch-wizard-1"]
     tags = {
-        Name = "JenkinsServer"
+        Name = "ELK-Stack"
     } 
-    user_data = file("docker-compose.yaml")
+    user_data =  <<EOF
+version: "3"
+services:
+    elasticsearch:
+        image: docker.elastic.co/elasticsearch/elasticsearch:7.10.0
+        environment:
+            - discovery.type=single-node
+        ports:
+            - 9200:9200
+    logstash:
+        image: docker.elastic.co/logstash/logstash:7.10.0
+        depends_on:
+            - elasticsearch
+    kibana:
+        image: docker.elastic.co/kibana/kibana:7.10.0
+        ports:
+            - 5601:5601
+        depends_on:
+            - elasticsearch
+EOF
+}
 
 }
